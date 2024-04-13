99f22e5d0b1543d5227c8af6be93f1b8f7787046
Luizefz <luizefsilva2@gmail.com>
2024-04-13 19:58:34 -0300

    feat: add logoHeader widget

--- pullebyte/lib/Screens/tela_cadastro.dart
+++ pullebyte/lib/Screens/tela_cadastro.dart
@@ -1,82 +1,95 @@
 // tela_cadastro.dart
 
 import 'package:flutter/material.dart';
+import 'package:pullebyte/CustomWidgets/logo_header.dart';
 import 'package:pullebyte/theme/colors.dart';
-import 'package:flutter_svg/flutter_svg.dart';  
+import 'package:flutter_svg/flutter_svg.dart';
 
 class TelaCadastro extends StatelessWidget {
   final String assetName = 'lib/Assets/Icon_cadastro.svg';
 
-  TelaCadastro({Key? key}) : super(key: key); // Adiciona um parâmetro de chave ao construtor
+  const TelaCadastro({super.key}); // Adiciona um parâmetro de chave ao construtor
 
   @override
   Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    // Calcula a posição horizontal desejada em porcentagem da tela (ex: 10%)
+    final leftPosition = screenWidth * 0.05;
+
     return Scaffold(
       backgroundColor: customColorScheme.background,
       appBar: AppBar(
-        backgroundColor: customColorScheme.primary,
-        title: const Text('Cadastro'), // Usa um construtor const
+        backgroundColor: Colors.transparent,
         centerTitle: true,
         leading: IconButton(
-          icon: const Icon(Icons.arrow_back), // Usa um construtor const
+          icon: const Icon(Icons.chevron_left_sharp), // Usa um construtor const
           onPressed: () {
             Navigator.pop(context);
           },
         ),
       ),
-      body: Column(
-  children: <Widget>[
-    Expanded(
-      child: SvgPicture.asset(
-        assetName,
-        fit: BoxFit.contain, // Usa BoxFit.contain para manter a proporção da imagem
-        semanticsLabel: 'Logo',
-      ),
-    ),
-    Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+      body: Stack(
         children: [
-          TextFormField(
-            style: TextStyle(color: customColorScheme.background),
-            textAlign: TextAlign.center,
-            decoration:  InputDecoration(
-              fillColor: customColorScheme.onPrimary,
-              filled: true,
-              labelText: 'Nome de Usuário',
-                  ),
+          Positioned(
+            top: 0,
+            left: leftPosition,
+            child: LogoHeader(),
+          ),
+          Column(
+            children: <Widget>[
+              Expanded(
+                child: SvgPicture.asset(
+                  assetName,
+                  fit: BoxFit.contain, // Usa BoxFit.contain para manter a proporção da imagem
+                  semanticsLabel: 'Logo',
                 ),
-                const SizedBox(height: 16),
-                TextFormField(
-                  style: TextStyle(color: customColorScheme.background),
-                  textAlign: TextAlign.center,
-                  decoration: InputDecoration(
-                    fillColor: customColorScheme.onPrimary,
-                    filled: true,
-                    labelText: 'E-mail',
-                  ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    TextFormField(
+                      style: TextStyle(color: customColorScheme.background),
+                      textAlign: TextAlign.center,
+                      decoration: InputDecoration(
+                        fillColor: customColorScheme.onPrimary,
+                        filled: true,
+                        labelText: 'Nome de Usuário',
+                      ),
+                    ),
+                    const SizedBox(height: 16),
+                    TextFormField(
+                      style: TextStyle(color: customColorScheme.background),
+                      textAlign: TextAlign.center,
+                      decoration: InputDecoration(
+                        fillColor: customColorScheme.onPrimary,
+                        filled: true,
+                        labelText: 'E-mail',
+                      ),
+                    ),
+                    const SizedBox(height: 16),
+                    TextFormField(
+                      style: TextStyle(color: customColorScheme.background),
+                      textAlign: TextAlign.center,
+                      obscureText: true, // Para esconder a senha
+                      decoration: InputDecoration(
+                        fillColor: customColorScheme.onPrimary,
+                        filled: true,
+                        labelText: 'Senha',
+                      ),
+                    ),
+                    const SizedBox(height: 24),
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, '/home');
+                      },
+                      child: const Text('Cadastrar'),
+                    ),
+                  ],
                 ),
-                const SizedBox(height: 16),
-                TextFormField(
-                  style: TextStyle(color: customColorScheme.background),
-                  textAlign: TextAlign.center,
-                  obscureText: true, // Para esconder a senha
-                  decoration: InputDecoration(
-                    fillColor: customColorScheme.onPrimary,
-                    filled: true,
-                    labelText: 'Senha',
-                  ),
-                ),
-                const SizedBox(height: 24),
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/home');
-                  },
-                  child: const Text('Cadastrar'),
-                ),
-              ],
-            ),
+              ),
+            ],
           ),
         ],
       ),
--- pullebyte/lib/main.dart
+++ pullebyte/lib/main.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:pullebyte/theme/colors.dart';
 import 'Screens/tela_cadastro.dart';
 import 'Screens/home.dart';
 
 void main() {
   runApp(MaterialApp(
+    title: 'Pullebyte',
     debugShowCheckedModeBanner: false,
+    theme: ThemeData(
+      colorScheme: customColorScheme,
+      useMaterial3: true,
+    ),
     initialRoute: '/tela_cadastro',
     routes: {
-      '/tela_cadastro': (context) => TelaCadastro(),
-      '/home': (context) => Home(),
+      '/tela_cadastro': (context) => const TelaCadastro(),
+      '/home': (context) => const Home(),
     },
   ));
-
+}
