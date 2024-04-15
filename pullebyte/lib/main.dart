8245e1ae2682f0c2ec038d7bed748088dba21c1b
Luizefz <luizefsilva2@gmail.com>
2024-04-14 21:42:11 -0300

    feat: add custom font to project

--- pullebyte/lib/main.dart
+++ pullebyte/lib/main.dart
@@ -1,14 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:pullebyte/Screens/tela_login.dart';
 import 'package:pullebyte/theme/colors.dart';
 import 'Screens/tela_cadastro.dart';
 import 'Screens/home.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(MaterialApp(
     title: 'Pullebyte',
     debugShowCheckedModeBanner: false,
     theme: ThemeData(
+      textTheme: GoogleFonts.poppinsTextTheme().apply(
+        bodyColor: customColorScheme.onPrimary,
+      ),
       colorScheme: customColorScheme,
       useMaterial3: true,
     ),
