15464e07c86c70e15702a04628462717010d231f
Luiz Eduardo <67990430+Luizefz@users.noreply.github.com>
2024-07-02 21:51:45 -0300

    fix: get geoToken - ok (#31)

--- pullebyte/lib/Screens/mapa.dart
+++ pullebyte/lib/Screens/mapa.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-//import 'package:pullebyte/keys/api_keys.dart';
 import 'package:mapbox_gl/mapbox_gl.dart';
 import 'package:mapbox_gl/mapbox_gl.dart' as mapbox;
 
@@ -25,7 +24,8 @@ class _MapaState extends State<Mapa> {
   }
 
   Future<void> _fetchGeoMapData(String stadiumName) async {
-    final String url = 'https://api.distancematrix.ai/maps/api/geocode/json?key=${String.fromEnvironment("GEO_ACCESS_TOKEN")}&address=$stadiumName';
+    const geoToken = String.fromEnvironment("GEO_ACCESS_TOKEN");
+    final String url = 'https://api.distancematrix.ai/maps/api/geocode/json?key=$geoToken&address=$stadiumName stadium';
     final response = await http.get(Uri.parse(url));
     final data = json.decode(utf8.decode(response.bodyBytes));
     setState(() {
@@ -51,5 +51,3 @@ class _MapaState extends State<Mapa> {
     );
   }
 }
-
- 
